48eca5d2bd0a680e1be79320481a062759f06379
Seongbae94 <seongbaelee944@gmail.com>
2023-11-12 15:40:19 -0500

    base setting
    base setting

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white">{`Hello, I'm Seongbae Lee`}</h1>;
+    </section>
+  );
+};
+
+export default HeroSection;
