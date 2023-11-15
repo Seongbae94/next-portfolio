f429b8ed655ea0cb1f38b048787e1dd1ec8cb5cb
Seongbae94 <seongbaelee944@gmail.com>
2023-11-14 22:09:28 -0500

    work on achievement section
    1. work on achievement section
    2. customizing color

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const ProjectTag = ({ name, onClick, isSelected }) => {
   const buttonSyles = isSelected
-    ? "text-white border-purple-500"
+    ? "text-white border-primary-500"
     : "text-[#ADB7BE] border-slate-600 hover:border-white";
 
   return (
     <button
       onClick={() => onClick(name)}
-      className={`rounded-full border-2  px-6 py-3 text-xl cursor-pointer ${buttonSyles}`}
+      className={`rounded-full border-2 px-6 py-3 text-xl cursor-pointer ${buttonSyles}`}
     >
       {name}
     </button>
