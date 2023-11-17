6aee2905bcde0443d628bddcedfef84a8876e4cc
Seongbae94 <seongbaelee944@gmail.com>
2023-11-16 22:57:37 -0500

    image issue solving
    image issue solving

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -9,15 +9,15 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
     <div>
       <div
         className="h-52 md:h-72 relative group"
-        // style={{ backgroundImage: `url(${imgUrl})`, backgroundSize: "cover" }}
+        style={{ backgroundImage: `url(${imgUrl})`, backgroundSize: "cover" }}
       >
-        <Image
+        {/* <Image
           src={imgUrl}
           layout="fill"
           objectFit="cover"
           className="rounded-t-xl"
           alt="project card"
-        />
+        /> */}
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
           <Link
             target="_blank"
